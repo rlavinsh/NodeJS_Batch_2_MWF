e5aa27bfbf80b39ec3a0fb2c87f843edfd37521b
rlavinsh <rlavinsh@gmail.com>
2026-07-17 19:32:33 +0530

    code updated

---
+++ Day-2/app.js
@@ -0,0 +1,12 @@
+const { log } = require("console");
+let os = require("os");
+// console.log(os);
+
+// console.log(os.arch());
+// console.log(os.cpus().length);
+// console.log(os.platform());
+// let totalRam = os.totalmem()/1024/1024/1024;
+// console.log(totalRam);
+// let freemem = os.freemem() / 1024 / 1024 / 1024;
+// console.log(freemem);
+// console.log(os.hostname());
