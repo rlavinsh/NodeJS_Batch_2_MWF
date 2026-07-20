9f9be8be2614f718a137eb69afb4e4704a79c087
rlavinsh <rlavinsh@gmail.com>
2026-07-21 00:11:40 +0530

    code updated

---
+++ Day-3/Task.md
@@ -0,0 +1,11 @@
+/student -> GET -> All Students Detail
+/student -> POST -> Student Created Successfully
+/ student -> PUT -> Student Detail Updated
+/student -> PATCH -> Student Name Updated
+/student -> DELETE -> Student Deleted
+
+/allBooks -> GET -> All Books Fetched
+/createBook ->POST -> Book Created Successfully
+/updateBook -> PUT ->Book Details Updated
+/updateBook-> PATCH ->Author Name Changed
+/deleteBook-> DELETE ->Book Deleted successfully
--- Day-3/app.js
+++ Day-3/app.js
@@ -3,14 +3,31 @@ let http = require("http");
 
 let server = http.createServer((req, res) => {
   //   console.log(req.url);
-  if (req.url == "/") {
-    res.end("Server running");
-  } else if (req.url == "/about") {
-    res.end("This is about page");
-  } else if (req.url == "/homepage") {
-    res.end("This is homepage");
+  // res.end(`<h1>server running</h1>`);
+  // console.log(req.url);
+  // console.log(req.method);
+
+  // if (req.url == "/") {
+  //   res.end("Server running");
+  // } else if (req.url == "/about") {
+  //   res.end("This is about page");
+  // } else if (req.url == "/homepage") {
+  //   res.end("This is homepage");
+  // } else {
+  //   res.end("No Route Found");
+  // }
+  if (req.url == "/student" && req.method == "GET") {
+    res.end("All Students Detail");
+  } else if (req.url == "/student" && req.method == "POST") {
+    res.end("Student Created Successfully");
+  } else if (req.url == "/student" && req.method == "PUT") {
+    res.end("Student Detail Updated");
+  } else if (req.url == "/student" && req.method == "PATCH") {
+    res.end("Student Name Updated");
+  } else if (req.url == "/student" && req.method == "DELETE") {
+    res.end("Student Deleted");
   } else {
-    res.end("No Route Found");
+    res.end(`<h1>No Route Found</h1>`);
   }
 });
 
