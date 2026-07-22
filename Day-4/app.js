753ef6830986d7d931b5fe8a7c20742369294a54
rlavinsh <rlavinsh@gmail.com>
2026-07-23 01:20:28 +0530

    code updated

---
+++ Day-4/app.js
@@ -0,0 +1,31 @@
+let http = require("http");
+
+let students = [
+  { id: 1, name: "Rahul", course: "Node" },
+  { id: 2, name: "Aman", course: "React" },
+];
+let server = http.createServer((req, res) => {
+  if (req.url == "/allStudents" && req.method === "GET") {
+    res.end(JSON.stringify(students));
+  } else if (req.url == "/createStudents" && req.method === "POST") {
+    let newStudent = { id: 3, name: "Priya", course: "Java" };
+    students.push(newStudent);
+    res.end("Student Added");
+  } else if (req.url == "/updateStudentDetail" && req.method === "PATCH") {
+    students[1].course = "MERN";
+    res.end("Course Updated");
+  } else if (req.url == "/updateStudent" && req.method === "PUT") {
+    students[1] = { id: 2, name: "Aman Kumar", course: "Backend" };
+    res.end("Student Replaced");
+  } else if (req.url == "/deleteStudent" && req.method === "DELETE") {
+    students.pop();
+    res.end("Student Deleted");
+  }
+  else{
+    res.end("Route Not Found")
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server running on port 3000");
+});
